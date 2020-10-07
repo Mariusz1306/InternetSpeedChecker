fb6d5a835a4ec6cd6670727d6a537ba67a7a4f71
Czajkowski Mariusz <Mariusz1306@gmail.com>
2020-10-07 21:12:48 +0200

    Added simple writing to a file
    Appends to (or creates) a file named [current_date].txt in a format:
    [current_date_and_time]
    Download: [speed]
    Upload: [speed]
    Ping: [ping]
    --------

--- InternetSpeedChecker.py
+++ InternetSpeedChecker.py
@@ -1,6 +1,7 @@
 import speedtest
+from datetime import datetime
 
-def checkspeed():
+def checkSpeed():
     s = speedtest.Speedtest()
     s.get_servers()
     s.get_best_server()
@@ -9,11 +10,27 @@ def checkspeed():
     
     return s.results
 
-def printresults(results):
+def printResults(results):
     print('Download: {:.2f} Mb/s\n'.format(results.download / 1024 / 1024))
     print('Upload: {:.2f} Mb/s\n'.format(results.upload / 1024 / 1024))
     print('Ping: ', results.ping)
 
+def fileWrite(results):
+    todays_date = datetime.now()
+    todays_date_full = todays_date.strftime("%Y-%m-%d %H:%M")
+    todays_date_simplified = todays_date.strftime("%Y-%m-%d")
+    
+    file = open(todays_date_simplified + ".txt", "a")
+    file.write(todays_date_full + "\n")
+    file.write('Download: {:.2f} Mb/s\n'.format(results.download / 1024 / 1024))
+    file.write('Upload: {:.2f} Mb/s\n'.format(results.upload / 1024 / 1024))
+    file.write('Ping: ' + str(results.ping) + '\n')
+    file.write("--------\n")
+    file.write("\n")
+    file.close()
+
 if __name__ == "__main__":
-    results = checkspeed()
-    printresults(results)
+    
+    results = checkSpeed()
+    printResults(results)
+    fileWrite(results)
